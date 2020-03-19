1cae4e17c186403dca55f11de946fe7e4a2142c1
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-03-19 23:09:34 +0100

    sample code

---
+++ Main.py
@@ -0,0 +1,27 @@
+import drone
+import sys
+import time
+
+
+drone_1 = drone.Drone("128.1.1.1",8889)
+
+
+##//print (drone.ip)
+
+drone_1.printinfo()
+
+drone_1.connect()
+
+drone_1.takeOff()
+time.sleep(1) 
+drone_1.cw("90")
+time.sleep(1)
+drone_1.ccw("360")
+drone_1.cw("180")
+time.sleep(1)
+print(drone_1.battery())
+drone_1.land()
+
+drone_1.end()
+
+
---
+++ drone.py
@@ -0,0 +1,65 @@
+import socket
+import sys
+import time
+
+
+class Drone(object):
+    """description of class"""
+
+    def __init__(self, ip,port):
+        self.TelloIp = ip
+        print("ip: " + ip)
+        self.TelloPort = port
+        # Create a UDP socket
+        self.Host =''
+        self.HostPort = 9000
+        self.locaddr = (self.Host,self.HostPort)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.tello_address = ('192.168.10.1', 8889)
+        self.sock.bind(self.locaddr)
+
+
+    def sendMessage(self,TelloMessage):
+        print("send message "+ TelloMessage +" end")
+        msg = TelloMessage.encode(encoding="utf-8")
+        sent = self.sock.sendto(msg,self.tello_address)
+        data, server = self.sock.recvfrcd om(1518)
+        #print("returværdi")
+        print(data.decode(encoding="utf-8"))
+        return "from sendmessage " + TelloMessage + " end "
+
+    def printinfo(self):
+        print("Hallo Drone at : "+ self.TelloIp)
+
+    def connect(self):
+        print("Connect")
+        result = self.sendMessage("command")
+        print (result)
+
+    def takeOff(self):
+        print("takeOff")
+        result = self.sendMessage("takeoff")
+
+    def land(self):
+        print("land")
+        result = self.sendMessage("land")
+
+    def end(self):
+        print("end")
+        self.sock.close()
+       
+    def cw(self,x):
+        print("cw")
+        r = self.sendMessage("cw "+x)
+
+    def ccw (self,x):
+        print("ccw")
+        r = self.sendMessage("ccw "+x)
+        
+    def battery(self):
+        r = self.sendMessage("battery?")
+        return r
+
+
+
+   
